f7e119a5ddd6c8cb8f5bb392d5475d33cefe50bc
pabloalanzahlut <zahlut87@yahoo.com.ar>
2018-03-28 21:26:13 -0300

    Se agrega mod pagos

---
+++ Pagos/FrmRecibo.vb
@@ -0,0 +1,19 @@
+﻿Public Class frmRecibo
+
+    Private Sub btnok_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub frmRecibo_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        btnImprimir.Focus()
+    End Sub
+
+   
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnImprimir.Click
+        btnImprimir.Visible = False
+        'Me.Focus()
+        PrintForm1.Print()
+
+    End Sub
+End Class
